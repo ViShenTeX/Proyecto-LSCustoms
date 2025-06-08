f37bd596c11566dc126d52d476e7e82b0af8ba90
LeVicharraca <vichosampson@gmail.com>
2025-06-08 14:01:19 -0400

    more things

--- Frontend/astro-frontend/astro.config.mjs
+++ Frontend/astro-frontend/astro.config.mjs
@@ -8,20 +8,6 @@ export default defineConfig({
     server: {
       port: 80,
       host: true,
-      proxy: {
-        '/admin': {
-          target: 'http://127.0.0.1:3000',
-          changeOrigin: true,
-        },
-        '/api': {
-          target: 'http://127.0.0.1:3000',
-          changeOrigin: true,
-        },
-        '/static': {
-          target: 'http://127.0.0.1:3000',
-          changeOrigin: true,
-        },
-      }
     }
   },
   devToolbar: {
