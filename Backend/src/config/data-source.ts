501bb72d29e21a5ae137bd71beb5b27032d6d4e8
LeVicharraca <vichosampson@gmail.com>
2025-06-08 23:05:48 -0400

    more fixes x3

--- Backend/src/config/data-source.ts
+++ Backend/src/config/data-source.ts
@@ -1,19 +1,20 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
-    port: parseInt(process.env.PORT || "3306"),
+    port: parseInt(process.env.DB_PORT || "3306"),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     synchronize: false,
     logging: true,
-    entities: ["src/models/**/*.ts"],
-    migrations: ["src/migrations/**/*.ts"],
-    subscribers: ["src/subscribers/**/*.ts"],
+    entities: [path.join(__dirname, "../models/**/*.{ts,js}")],
+    migrations: [path.join(__dirname, "../migrations/**/*.{ts,js}")],
+    subscribers: [path.join(__dirname, "../subscribers/**/*.{ts,js}")]
 }); 
